b1e4dd147740dda2cdf6d73d25c32748d903abe8
Michael Senter, PhD <dmsenter89@users.noreply.github.com>
2024-07-25 03:25:17 +0000

    feat: iterate over userIDs in JSON

--- src/parse.c
+++ src/parse.c
@@ -74,6 +74,31 @@ static int get_cjson_int(cJSON* json, char* key)
     }
 }
 
+Staff* Staff_init(ptrdiff_t capacity)
+{
+    Staff* out = malloc(sizeof(Staff));
+    out->member = malloc(sizeof(Employee)*capacity);
+    out->len = 0;
+    out->cap = capacity;
+    
+    return out;
+}
+
+Staff* parse_staff_json(cJSON* root)
+{
+    Staff* out = Staff_init(10);
+    cJSON* user = 0;
+    cJSON_ArrayForEach(user, root) {
+        char* userID = user->string;
+        if (userID) {
+            Employee* emp = parse_employee_json(user, userID);
+            out->member[out->len++] = *emp;
+        }
+    }
+
+    return out;
+}
+
 #ifndef TESTING
 int main(int argc,  char* argv[argc+1])
 {
@@ -109,6 +134,8 @@ void test_parse_employee_json_sale(void)
     for (size_t i=0; i<3; ++i) {
         TEST_CHECK(emp->sales[i] == sales[i]);
     }
+
+    cJSON_Delete(json);
 }
 
 void test_parse_employee_json_nosale(void)
@@ -125,11 +152,45 @@ void test_parse_employee_json_nosale(void)
     TEST_CHECK(strcmp(emp->name, "Jennifer Barnes") == 0);
     TEST_CHECK(emp->sales_count == 0);
     TEST_CHECK(emp->sales == 0);
+
+    cJSON_Delete(json);
+}
+
+void test_iterate_employee_recs(void)
+{
+    const char *json_string =
+        "{"
+        "   \"dangre\" : {"
+        "      \"name\" : \"Daniel Green\","
+        "      \"department\" : 4,"
+        "      \"sales\" : [ 4, 3, 7 ]"
+        "   },"
+        "   \"jenbar\" : {"
+        "       \"name\" : \"Jennifer Barnes\","
+        "       \"department\" : 1"
+        "}}";
+    cJSON* json = cJSON_Parse(json_string);
+    TEST_CHECK(!cJSON_IsInvalid(json));
+
+    Staff* staff = parse_staff_json(json);
+
+    TEST_CHECK(staff->len == 2);
+
+    TEST_CHECK(strcmp(staff->member[0].userID, "dangre") == 0);
+    TEST_MSG("Expected: '%s'", "dangre");
+    TEST_MSG("Produced: '%s'", staff->member[0].userID);
+
+    TEST_CHECK(strcmp(staff->member[1].userID, "jenbar") == 0);
+    TEST_MSG("Expected: '%s'", "jenbar");
+    TEST_MSG("Produced: '%s'", staff->member[1].userID);
+
+    cJSON_Delete(json);
 }
 
 TEST_LIST = {
    { "employee parsing - with sale", test_parse_employee_json_sale},
    { "employee parsing - no sales", test_parse_employee_json_nosale},
+   { "iterate over multiple emp recs", test_iterate_employee_recs},
    { NULL, NULL }     /* zeroed record marking the end of the list */
 };
 #endif
--- src/parse.h
+++ src/parse.h
@@ -9,9 +9,17 @@ typedef struct {
     size_t sales_count;
 } Employee;
 
+typedef struct {
+    Employee* member;
+    ptrdiff_t len;
+    ptrdiff_t cap;
+} Staff;
+
 Employee* Employee_new(char* userID);
 void Employee_free(Employee* in);
 
 Employee* parse_employee_json(cJSON* json, char* userID);
+
+Staff* Staff_init(ptrdiff_t capacity);
 
 #endif
